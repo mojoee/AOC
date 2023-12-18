d645e150980c22181bd83e095648db822b124de5
mojoee <47809126+mojoee@users.noreply.github.com>
2023-12-18 16:02:19 +0100

    day10 part2

--- 2023/day10/main.py
+++ 2023/day10/main.py
@@ -19,7 +19,11 @@ distance = 0
 start_coords = (25, 77)
 next_coords = (24, 77)
 not_finished = True
+x_coordinates = []
+y_coordinates = []
 while not_finished:
+    x_coordinates.append(start_coords[0])
+    y_coordinates.append(start_coords[1])
     direction = arr[next_coords[0]][next_coords[1]]
     sodNorthSouth = next_coords[0]-start_coords[0]
     sodEastWest = next_coords[1]-start_coords[1]
@@ -49,4 +53,35 @@ while not_finished:
     elif direction == "S":
         not_finished = False
     distance += 1
+    # arr[next_coords[0]][next_coords[1]] = 1
 print(distance/2)
+
+
+# part 2
+
+def shoelace_formula(x_coords, y_coords):
+    """
+    Calculate the area of a polygon using the Shoelace Formula.
+
+    Parameters:
+    - x_coords: List of x-coordinates of vertices.
+    - y_coords: List of y-coordinates of vertices.
+
+    Returns:
+    - Area of the polygon.
+    """
+    n = len(x_coords)
+
+    # Check if the number of x-coordinates is equal to the number of y-coordinates
+    if n != len(y_coords):
+        raise ValueError("Number of x-coordinates must be equal to the number of y-coordinates.")
+
+    # Apply the Shoelace Formula
+    area = 0.5 * abs(sum(x_coords[i] * y_coords[(i + 1) % n] - x_coords[(i + 1) % n] * y_coords[i] for i in range(n)))
+
+    return area
+
+area = shoelace_formula(x_coordinates, y_coordinates)
+print("Area of the polygon:", area)
+interiorPoints = area - distance/2 + 1
+print(interiorPoints)
